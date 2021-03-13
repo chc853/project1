15d7171888d3336dcfdb8880a999b8bd8d390590
chc85 <chc853@gmail.com>
2021-03-13 23:33:00 +0900

    Hello Spring CRDU API 개발

--- src/test/java/com/chc853/book/springboot/HelloControllerTest.java
+++ src/test/java/com/chc853/book/springboot/HelloControllerTest.java
@@ -10,11 +10,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+//@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -26,5 +27,16 @@ public class HelloControllerTest {
         ResultActions resultActions = mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name).param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
